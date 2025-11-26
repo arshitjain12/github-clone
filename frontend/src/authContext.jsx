ee42f35a99aa55967eba817826d7ab7039123c60
arshit jain <arshitjain7@gmail.com>
2025-11-26 18:52:27 +0530

    Signup frontend created

---
+++ frontend/src/authContext.jsx
@@ -0,0 +1,20 @@
+import { useState, useEffect, useContext, createContext } from "react";
+const AuthContext = createContext();
+
+export const useAuth = () => useContext(AuthContext);
+
+export const AuthProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(null);
+  useEffect(() => {
+    const userId = localStorage.getItem("userId");
+    if (userId) {
+      setCurrentUser(userId);
+    }
+  }, []);
+
+  const value = {
+    currentUser,
+    setCurrentUser,
+  };
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+};
